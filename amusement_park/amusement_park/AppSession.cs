ad7921dd8ee4ed4ea8e868e553150b3e8cdbd917
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-09-24 21:35:44 +0300

    add obj forms

--- amusement_park/amusement_park/AppSession.cs
+++ amusement_park/amusement_park/AppSession.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using amusement_park.view;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,17 @@ namespace amusement_park
     {
         public static bool IsLoggedIn { get; set; } // Общая переменная для проверки входа
         public static string UserLogin { get; set; } // логин залогиненного юзера
+        public static TableAttractionsForm AttractionsForm { get; set; }
+        public static UserForm userForm { get; set; }
+        public static TableUsersForm tableUsersForm { get; set; }
+        public static TableAttractionsForm attractionsForm { get; set; }
+        public static RegForm regForm { get; set; }
+        public static LoginForm loginForm { get; set; }
+        public static ChangePasswordForm changePasswordForm { get; set; }
+        public static BuyTicketForm ticketForm { get; set; }
+        public static AttractionsViewForm attractionsViewForm { get; set; }
+        public static AdminForm adminForm { get; set; }
+
     }
 
 }
